8afee8b42e30c876befab34614e7fb832ecb7554
rickymwethera <rickymwethera@gmail.com>
2021-01-19 21:20:11 +0300

    added django registration

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('instaapp.urls'))
+    path('', include('instaapp.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

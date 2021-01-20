8a9b9a4b844a81958ecf5fc24cd45a8abb26b8fa
rickymwethera <rickymwethera@gmail.com>
2021-01-20 03:46:24 +0300

    added forms.py

---
+++ instaapp/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Image, Comment, Profile
+
+class PostForm(forms.ModelForm):
+    class Meta:
+
+        model = Image
+
+        fields = [
+            "image",
+            "name",
+            "caption",
+        ]
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+
+        fields = [
+            "comment",
+        ]
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = [
+            "profile_picture",
+            "bio",
+        ]

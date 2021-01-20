011d30718cf9acb440dfb35f7878b39c81c12901
rickymwethera <rickymwethera@gmail.com>
2021-01-20 18:21:39 +0300

    added media roots

--- instagram/settings.py
+++ instagram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'instaapp.apps.InstaappConfig',
     'django_registration',
     'bootstrap3',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'

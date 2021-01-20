5d6e12bc317b97d4924abc6d5f7f7ef5769b8a16
rickymwethera <rickymwethera@gmail.com>
2021-01-20 18:24:14 +0300

    added more urls

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
+    path('post/new/',views.create_post,name='create'),
+    url(r'^post/(\d+)',views.detail,name = 'detail'),
+    path('profile/', views.profile, name='profile'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

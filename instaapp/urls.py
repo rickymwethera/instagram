cb659caf8a703e98b5bb24470a5efe8de06b79b6
rickymwethera <rickymwethera@gmail.com>
2021-01-20 03:28:11 +0300

    import paths

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns=[
-    url('',views.welcome,name = 'welcome'),
+    path('',views.welcome,name = 'welcome'),
 ]

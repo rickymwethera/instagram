7ea9f54bb3705dc6271df6f9ede52895b9c516c1
rickymwethera <rickymwethera@gmail.com>
2021-01-21 00:51:34 +0300

    deployment process

--- instaapp/tests.py
+++ instaapp/tests.py
@@ -1,3 +1,88 @@
+
 from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Image, Profile,Comment
+
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    '''
+    test class for Profile model
+    '''
+    def setUp(self):
+        self.user = User.objects.create_user("vector", "pass")
+        self.profile_test = Profile(profile_picture='images/default.png',
+                                            bio="test bio",
+                                            user=self.user)
+        self.profile_test.save()
+
+    def test_instance_true(self):
+        self.profile_test.save()
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+
+class ImageTestClass(TestCase):
+    """test class for Image model"""
+
+    def setUp(self):
+
+        self.user = User.objects.create_user("vector", "pass")
+
+        self.new_profile = Profile(profile_picture='images/default.png',bio="test bio",
+                                     user=self.user)
+        self.new_profile.save()
+
+        self.new_image = Image(image='images/ford.png',
+                               caption="image", name='ford', author=self.user)
+
+    def test_instance_true(self):
+        self.new_image.save()
+        self.assertTrue(isinstance(self.new_image, Image))
+
+    def test_save_image_method(self):
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+
+
+
+class CommentTestClass(TestCase):
+
+    """Test class for Comment Model"""
+
+    def setUp(self):
+        self.new_user = User.objects.create_user("vector", "pass")
+
+        self.new_profile = Profile(profile_picture='images/default.png',
+                                     bio="test bio",
+                                     user=self.new_user)
+        self.new_profile.save()
+
+        self.new_image = Image(pic='images/ford.jpg',
+                               caption="test image",author=self.new_user)
+        self.new_image.save()
+
+        self.new_comment = Comment(
+            image=self.new_image, name=self.new_profile, comment="this is a  test comment on a post")
+
+    def test_instance_true(self):
+        self.new_comment.save()
+        self.assertTrue(isinstance(self.new_comment, Comment))
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments) == 1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        Comment.objects.all().delete()
--- instagram/settings.py
+++ instagram/settings.py
@@ -12,19 +12,24 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
+
+MODE=config("MODE", default="dev")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e3$xunkz5yvs^^^_)wgxt_9)hqe_pj8ed%=_o&@f#_cs(5(^fg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,15 +84,31 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+if config('MODE')=="dev":
+       DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instagram',
-        'USER': 'ricky',
-    'PASSWORD':'mwethera99',
-    }
-}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 
 # Password validation
@@ -135,7 +157,12 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
+
+
+django_heroku.settings(locals())
